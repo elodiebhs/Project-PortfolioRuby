3684a3fac07cfcc1b1e85d089e208047e2ee3472
elodiebhs <elodiebouthors@hotmail.com>
2021-09-16 21:36:56 +0000

    set the welcome action as the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #set the welcome action as the root route
+  root 'pages#welcome'
 end
